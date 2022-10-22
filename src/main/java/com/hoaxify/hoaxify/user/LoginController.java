d8a59b202651ec486dfd0ec88306d9cda5b321fd
ylliu <liuming1908@gmail.com>
2022-10-22 11:20:17 +0800

    35.Authenticated User

--- src/main/java/com/hoaxify/hoaxify/user/LoginController.java
+++ src/main/java/com/hoaxify/hoaxify/user/LoginController.java
@@ -1,13 +1,18 @@
 package com.hoaxify.hoaxify.user;
 
+import com.hoaxify.hoaxify.shared.CurrentUser;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
     @PostMapping("/api/1.0/login")
-    void handleLogin() {
-
+    Map<String, Object> handleLogin(@CurrentUser User loggedInUser) {
+        return Collections.singletonMap("id", loggedInUser.getId());
     }
 }
--- src/test/java/com/hoaxify/hoaxify/LoginControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/LoginControllerTest.java
@@ -10,11 +10,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -78,7 +83,17 @@ public class LoginControllerTest {
         authenticate();
         ResponseEntity<Object> response = login(Object.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
+    @Test
+    public void postLogin_withValidCredentials_receiveLoggedInUserId() {
+        User inDB = userService.save(TestUtil.createValidUser());
+        authenticate();
+        ResponseEntity<Map<String, Object>> response = login(new ParameterizedTypeReference<Map<String, Object>>() {
+        });
+        Map<String, Object> body = response.getBody();
+        Integer id = (Integer) body.get("id");
+        assertThat(id).isEqualTo(inDB.getId());
     }
 
     private boolean authenticate() {
@@ -87,5 +102,10 @@ public class LoginControllerTest {
 
     public <T> ResponseEntity<T> login(Class<T> responseType) {
         return testRestTemplate.postForEntity(API_1_0_LOGIN, null, responseType);
+    }
+
+
+    public <T> ResponseEntity<T> login(ParameterizedTypeReference<T> responseType) {
+        return testRestTemplate.exchange(API_1_0_LOGIN, HttpMethod.POST, null, responseType);
     }
 }
